25d3e6bbc499d86b1fe622efa9b6966e60e5c716
dewakuneiei <dewakuneiei@gmail.com>
2023-07-13 10:01:55 +0700

    Quiz1 Update v1
    Added more showing of the detail.

--- Quiz1.java
+++ Quiz1.java
@@ -1,12 +1,28 @@
 public class Quiz1 {
     public static void main(String[] args) {
-        Student studentA = new Student("Owen", 2565, 3.84f);
+        Student studentA = new Student("Owen", 2565, 1.75f);
+        Student studentB = new Student("Plug", 2564, 3.17f);
+        Student studentC = new Student("Thanet", 2566, 2.75f);
+
+        System.out.println("--Initailize--");
         studentA.ShowDetails();
+        studentB.ShowDetails();
+        studentC.ShowDetails();
+        
+        System.out.println("--Modification--");
+        studentA.SetGPA(3.98f);
+        studentA.UpdateAdmisYear(1);
+        studentB.UpdateAdmisYear(2);
+        studentC.SetName("Jin Mori");
+
+        for (Student student : new Student[] {studentA, studentB, studentC}) {
+            student.ShowDetails();
+        }
     }
 }
 
 class Student {
-    private static int seed = 11001100;
+    private static int seed = 100;
 
     private int id;
     private String name;
@@ -19,8 +35,7 @@ class Student {
     }
 
     public void ShowDetails() {
-        
-        System.out.println( String.format("***\nID: %d\nName: %s\nAddmission Year: %d\nGPA: %s", 
+        System.out.println( String.format("****** Show Details ******\nID: %d\nName: %s\nAddmission Year: %d\nGPA: %s\n", 
         this.id, this.name,this.addmissionYear,String.format("%.02f", this.gpa)));
     }
 
@@ -32,11 +47,11 @@ class Student {
         this.name = newName;
     }
 
-    public void SetAddmission(int year) {
-        this.addmissionYear = year;
+    public void UpdateAdmisYear(int nextNumber) {
+        this.addmissionYear += nextNumber;
     }
 
-    public void SetGPA(int newGPA) {
+    public void SetGPA(float newGPA) {
         this.gpa = newGPA;
     }
 }
